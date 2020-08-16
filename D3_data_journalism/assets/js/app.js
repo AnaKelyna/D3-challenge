33b153a1925c97f9a87d060e054abb3ab001bb13
AnaKelyna <64177211+AnaKelyna@users.noreply.github.com>
2020-08-15 22:35:02 -0500

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -84,18 +84,23 @@ d3.csv("data.csv").then(function(censusData) {
     // Step 6: Initialize tool tip
     // ==============================
     // Add state labels to the points
-    var circleLabels = circlesGroup.selectAll("text").data(censusData).enter().append("text");
+    var circleLabels = chartGroup.selectAll(null).data(censusData).enter().append("text");
 
     circleLabels
-        .attr("x", function(d) { return d.poverty; })
-        .attr("y", function(d) { return d.healthcare; })
-        .text(function(d) { return d.abbr; })
+        .attr("x", function(d) {
+            return xLinearScale(d.poverty);
+        })
+        .attr("y", function(d) {
+            return yLinearScale(d.healthcare);
+        })
+        .text(function(d) {
+            return d.abbr;
+        })
         .attr("font-family", "sans-serif")
-        .attr("font-size", "5px")
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
         .attr("fill", "white");
 
-    // Step 7: Create tooltip in the chart
-    // ==============================
     // Create axes labels
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
@@ -103,13 +108,11 @@ d3.csv("data.csv").then(function(censusData) {
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .attr("class", "axisText")
-        .style("text-anchor", "middle")
         .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
         .attr("class", "axisText")
-        .style("text-anchor", "middle")
         .text("In Poverty (%)");
 
     // Initialize tooltip
